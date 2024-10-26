b34fea98e2d8074dc9b57fb20296f9d762fffb90
ChiCongn <chicongvn1205@gmail.com>
2024-10-26 09:18:19 +0700

    build: add Google API Client libraries for Google Books API

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,10 @@ module edu.lms {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
+    
+    requires com.google.api.client;
+    requires okhttp3;
+    requires com.fasterxml.jackson.databind;
 
     opens edu.lms to javafx.fxml;
     exports edu.lms;
