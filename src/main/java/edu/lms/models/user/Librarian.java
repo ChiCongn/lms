0dfac82f9549bbfe1354a0921aaeb30853aaa981
ChiCongn <chicongvn1205@gmail.com>
2024-11-07 15:20:00 +0700

    feat: create admin and librarian

---
+++ src/main/java/edu/lms/models/user/Librarian.java
@@ -0,0 +1,7 @@
+package edu.lms.models.user;
+
+public class Librarian extends User {
+    public Librarian(int id, String username, String password, String avatar) {
+        super(id, username, password, avatar);
+    }
+}
