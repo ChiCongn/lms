99fabac0ce168c7d3de13ac8f4842d1c769003ed
dodat17 <dat170405@gmail.com>
2024-11-07 12:44:07 +0700

    feat: adding fxml file for testing purpose

--- src/main/java/edu/lms/Controllers/LoginController.java
+++ src/main/java/edu/lms/Controllers/LoginController.java
@@ -1,4 +1,26 @@
 package edu.lms.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOError;
+import java.io.IOException;
+
 public class LoginController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToDashBoard(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("DashBoard.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
