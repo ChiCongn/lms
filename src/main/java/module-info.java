44462c6bc48b6681599100973a64cd6deab5d782
ChiCongn <chicongvn1205@gmail.com>
2024-11-08 14:58:38 +0700

    build: change dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,20 +2,21 @@ module edu.lms {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-
-    requires com.google.api.client;
-    requires okhttp3;
-    requires com.fasterxml.jackson.databind;
-    requires com.google.gson;
-
     requires java.dotenv;
 
+    requires org.controlsfx.controls;
+
+    requires okhttp3;
+    requires com.google.gson;
+
     requires java.sql;
+    requires mysql.connector.j;
 
     requires java.mail;
+    requires java.base;
 
     opens edu.lms to javafx.fxml;
     exports edu.lms;
+    exports edu.lms.controllers;
+    opens edu.lms.controllers to javafx.fxml;
 }
