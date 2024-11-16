90ad7efe3c5abadbcc67a4c0f90a3427a7c6c2e6
ChiCongn <chicongvn1205@gmail.com>
2024-11-16 23:57:27 +0700

    feat: add add delete, suspend and reactivate user method

--- src/main/java/edu/lms/controllers/dashboard/ClientsManagementController.java
+++ src/main/java/edu/lms/controllers/dashboard/ClientsManagementController.java
@@ -4,18 +4,19 @@ import edu.lms.Constants;
 import edu.lms.controllers.SceneManager;
 import edu.lms.models.user.Client;
 import edu.lms.models.user.UserManager;
+import edu.lms.services.AlertDialog;
+import edu.lms.services.database.UsersDataService;
+import javafx.animation.PauseTransition;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
 import java.math.BigDecimal;
 import java.net.URL;
@@ -43,6 +44,18 @@ public class ClientsManagementController implements Initializable {
     @FXML
     private TableColumn<Client, String> statusColumn;
 
+    @FXML
+    private Label selectionClientWarningLabel;
+
+    @FXML
+    private Button activateButton;
+
+    @FXML
+    private Button suspendButton;
+
+    @FXML
+    private Button deleteButton;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -57,7 +70,6 @@ public class ClientsManagementController implements Initializable {
                     setGraphic(null);
                 } else {
                     Client client = getTableRow().getItem();
-
                     if (client != null) {
                         ImageView avatar = new ImageView(new Image(client.getAvatarPath()));
                         avatar.setFitWidth(20);
@@ -67,7 +79,14 @@ public class ClientsManagementController implements Initializable {
                         // Style HBox
                         HBox hbox = new HBox(10, avatar, username);
                         hbox.setAlignment(Pos.CENTER_LEFT);
-
+                        hbox.setOnMouseClicked(mouseEvent -> {
+                            if (mouseEvent.getClickCount() == 1) {
+                                Client selectedUser = clientsTableView.getSelectionModel().getSelectedItem();
+                                if (selectedUser != null) {
+                                    switchToClientDetails(selectedUser);
+                                }
+                            }
+                        });
                         setGraphic(hbox);
                     }
                 }
@@ -79,14 +98,90 @@ public class ClientsManagementController implements Initializable {
         statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
         clientsTableView.setItems(clients);
 
-        clientsTableView.setOnMouseClicked(mouseEvent -> {
-            if (mouseEvent.getClickCount() == 1) {
-                Client selectedUser = clientsTableView.getSelectionModel().getSelectedItem();
-                if (selectedUser != null) {
-                    switchToClientDetails(selectedUser);
-                }
+        initializeButtonActions();
+    }
+
+    private void initializeButtonActions() {
+        handleDeleteAction();
+        handleSuspendAction();
+        handleActivateAction();
+    }
+
+    private void handleSuspendAction() {
+        suspendButton.setOnAction(event -> {
+            Client selectedClient = clientsTableView.getSelectionModel().getSelectedItem();
+            if (isSelectClient(selectedClient)) {
+                if (selectedClient.getStatus().equals("suspended")) return;
+                Alert confirmation = AlertDialog.makeConfirmationAlert("confirmation",
+                        "Warning: Suspending this client will revoke their access. Proceed with caution");
+                confirmation.showAndWait().ifPresent(response -> {
+                    if (response == ButtonType.OK) {
+                        if (!UsersDataService.suspendUser(selectedClient.getId())) {
+                            System.err.println("something is wrong. Can not suspend user!");
+                        }
+                        System.out.println("suspend");
+                    } else {
+                        System.out.println("cancel");
+                    }
+                });
             }
         });
+    }
+
+    private void handleActivateAction() {
+        activateButton.setOnAction(event -> {
+            Client selectedClient = clientsTableView.getSelectionModel().getSelectedItem();
+            if (isSelectClient(selectedClient)) {
+                if (selectedClient.getStatus().equals("active")) return;
+                Alert confirmation = AlertDialog.makeConfirmationAlert("confirmation",
+                        "Warning: Activating this client will restore their account. Proceed carefully");
+                confirmation.showAndWait().ifPresent(response -> {
+                    if (response == ButtonType.OK) {
+                        if (!UsersDataService.reactivateUser(selectedClient.getId())) {
+                            System.err.println("Something is wrong. Can not reactivate user!");
+                        }
+                        System.out.println("active");
+                    } else {
+                        System.out.println("cancel");
+                    }
+                });
+            }
+        });
+    }
+
+    private void handleDeleteAction() {
+        deleteButton.setOnAction(event -> {
+            Client selectedClient = clientsTableView.getSelectionModel().getSelectedItem();
+            if (isSelectClient(selectedClient)) {
+                Alert confirmation = AlertDialog.makeConfirmationAlert("confirmation",
+                        "Warning: Deleting this client will remove all associated data. Are you sure?");
+                confirmation.showAndWait().ifPresent(response -> {
+                    if (response == ButtonType.OK) {
+                        clientsTableView.getItems().remove(selectedClient);
+                        if (!UsersDataService.deleteUser(selectedClient.getId())) {
+                            System.err.println("Something is wrong. Can not delete user!");
+                        }
+                    } else {
+                        System.out.println("cancel");
+                    }
+                });
+            }
+        });
+    }
+
+    private boolean isSelectClient(Client selectedClient) {
+        if (selectedClient != null) {
+            selectionClientWarningLabel.setVisible(false);
+            return true;
+        } else {
+            selectionClientWarningLabel.setVisible(true);
+
+            //hide the label after 2 seconds
+            PauseTransition hideLabel = new PauseTransition(Duration.seconds(2));
+            hideLabel.setOnFinished(e -> selectionClientWarningLabel.setVisible(false));
+            hideLabel.play();
+            return false;
+        }
     }
 
     private void switchToClientDetails(Client client) {
